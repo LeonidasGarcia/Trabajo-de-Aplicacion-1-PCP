dacb0c31a7b48a2f41e267a18bde7b44473c8770
LeonidasGarcia <leo.garcia.dev.ts@gmail.com>
2026-09-10 23:04:14 -0500

    Refactor GeneradorDataset to use a constant for the dataset filename and simplify file writing logic by removing unnecessary buffer handling.

--- src/generador/GeneradorDataset.java
+++ src/generador/GeneradorDataset.java
@@ -1,9 +1,9 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 
 public class GeneradorDataset {
+
+    public static final String NOMBRE_ARCHIVO = "dataset.dat";
 
     public void generar(ParametrosGeneracion parametros) throws IOException {
         int N = parametros.N();
@@ -11,23 +11,12 @@ public class GeneradorDataset {
         int A = parametros.A();
         int B = parametros.B();
 
-        try (RandomAccessFile archivo = new RandomAccessFile("dataset.dat", "rw")) {
+        try (RandomAccessFile archivo = new RandomAccessFile(NOMBRE_ARCHIVO, "rw")) {
             archivo.setLength(0);
-            FileChannel canal = archivo.getChannel();
-            ByteBuffer buffer = ByteBuffer.allocate(1 << 16);
             for (int i = 0; i < N; i++) {
                 for (int d = 0; d < n; d++) {
-                    buffer.putInt(GeneradorCoordenadas.generar(A, B));
-                    if (!buffer.hasRemaining()) {
-                        buffer.flip();
-                        canal.write(buffer);
-                        buffer.clear();
-                    }
+                    archivo.writeInt(GeneradorCoordenadas.generar(A, B));
                 }
-            }
-            buffer.flip();
-            while (buffer.hasRemaining()) {
-                canal.write(buffer);
             }
         }
     }
