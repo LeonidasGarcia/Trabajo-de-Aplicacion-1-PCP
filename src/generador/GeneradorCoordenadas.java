0910f14d9ec95b353fd0e87f7c53013e2891489c
LeonidasGarcia <leo.garcia.dev.ts@gmail.com>
2026-09-10 23:43:51 -0500

    Add comments to clarify logic in GeneradorCoordenadas class

--- src/generador/GeneradorCoordenadas.java
+++ src/generador/GeneradorCoordenadas.java
@@ -1,7 +1,10 @@
 public final class GeneradorCoordenadas {
     public static int generar(int limiteInferior, int limiteSuperior) {
+        // Casteo a long para evitar overflow silencioso
         long rango = (long) limiteSuperior - limiteInferior + 1;
         long desvio = (long) (Math.random() * rango);
+
+        // el desvio esta acotado entre B - A o menor a este valor entonces el valor retornado a lo mas es B
         return (int) (limiteInferior + desvio);
     }
 }
